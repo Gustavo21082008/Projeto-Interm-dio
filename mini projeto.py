2449041294e1d99595c0bf8cbfa3487c560463cb
Gustavo21082008 <shark06874@sharkcoders.pt>
2025-01-11 12:50:44 +0000

    Update mini projeto.py

--- mini projeto.py
+++ mini projeto.py
@@ -1,30 +1,29 @@
-stock_total = ["arroz","feijão","Farofa"]
-preço1 = ([15,14,10])
-Quantidade = (["Quantidade:35","Quantidade:40","Quantidade 100"])
+produtos = ["arroz","feijão","Farofa"]
+preço1 = [15,14,10]
+Quantidade = [35,40,100]
 carrinho_produtos = []
 carrinho_quantidade = []
 def ver_stock():
- print("stock_total",stock_total,)
+ print("produtos",produtos,)
  print("preço",preço1)
  print("quantidade",Quantidade)
 def Comprar_Produto():
     produto = input("qual produto deseja adicionar ao carrinho")
-    if produto not in stock_total:
+    if produto not in produtos:
         print("esse produto não existe")
     else:
         indice = produto.index(produto)
+        quantidade = int(input("Quantos produtos deseja adicionar"))
+
+        if quantidade > Quantidade[indice]:
+            print("quantidade insuficiente no stock total!")
         if produto not in carrinho_produtos:
-         (carrinho_produtos.append(produto))
-    quantidade = int(input("quantos produtos deseja adicionar"))
-    if quantidade > Quantidade[indice]:
-        print("quantidade insuficiente no stock total!")
-        carrinho_quantidade.append(Quantidade)
-
-        stock_total[indice] -= quantidade
-        print("produto adicionado com sucesso")
-    else:
-        carrinho_quantidade[indice] += quantidade
-
+            carrinho_produtos.append(produto)
+            carrinho_quantidade.append(quantidade)
+        else:
+            carrinho_quantidade[indice] += quantidade
+        Quantidade[indice] -= quantidade
+        print("adicionado com sucesso")
 def Remover_do_carrinho():
     print("")
 
